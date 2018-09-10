54b963e3b3e8b907d592c7ac5137c0a03e61c086
Frederic Merizen <frederic.merizen@gmail.com>
2018-09-10 16:27:55 +0200

    #18 Bump Clojure
    In order to be able to bump ClojureScript

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/weavejester/reagi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.298.0-2a82a1-alpha"]
                  [org.clojure/clojurescript "0.0-2202"]]
   :plugins [[codox "0.6.7"]
