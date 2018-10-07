c1f45354e9a6c9059c18b841708255575d69cae7
Frederic Merizen <frederic.merizen@gmail.com>
2018-10-07 19:03:46 +0200

    #28 jdk6, but with no cljs
    because google closure compiler compiler needs at least java 7

--- project.clj
+++ project.clj
@@ -3,12 +3,8 @@
   :url "https://github.com/weavejester/reagi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.3.465"]
-                 [org.clojure/clojurescript "1.7.28"]
-                 [eftest "0.5.3"][clj-async-test "0.0.5"]]
+  :dependencies [[org.clojure/core.async "0.3.465"]]
   :plugins [[lein-codox "0.10.4"]
-            [lein-cljsbuild "1.1.7"]
             [lein-eftest "0.5.3"]]
   :source-paths ["src/cljc"]
   :test-paths ["test/clojure"]
@@ -18,32 +14,36 @@
              :compiler {:output-to "target/main.js"}}]}
   :eftest {:capture-output? false}
   :profiles
-  {:1.7  {}
-   :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]
-                         [org.clojure/clojurescript "1.8.51"]]}
-   :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]
-                         [org.clojure/clojurescript "1.9.946"]]}
-   :1.10 {:dependencies [[org.clojure/clojure "1.10.0-alpha8"]
-                         [org.clojure/clojurescript "1.10.339"]]}
+  {:cljs {:plugins [[com.cemerick/austin "0.1.6"]
+                    [lein-cljsbuild "1.1.7"]
+                    [lein-doo "0.1.10"]]}
+   :clj-1.7   {:dependencies [[org.clojure/clojure        "1.7.0"        :scope "provided"]]}
+   :cljs-1.7  {:dependencies [[org.clojure/clojurescript  "1.7.28"       :scope "provided"]]}
+   :clj-1.8   {:dependencies [[org.clojure/clojure        "1.8.0"        :scope "provided"]]}
+   :cljs-1.8  {:dependencies [[org.clojure/clojurescript  "1.8.51"       :scope "provided"]]}
+   :clj-1.9   {:dependencies [[org.clojure/clojure        "1.9.0"        :scope "provided"]]}
+   :cljs-1.9  {:dependencies [[org.clojure/clojurescript  "1.9.946"      :scope "provided"]]}
+   :clj-1.10  {:dependencies [[org.clojure/clojure       "1.10.0-alpha8" :scope "provided"]]}
+   :cljs-1.10 {:dependencies [[org.clojure/clojurescript "1.10.339"      :scope "provided"]]}
    :with-xml { ; Needed for cljs < 1.10 to work with java >= 9
               :jvm-opts ~(let [version     (System/getProperty "java.version")
                                [major _ _] (clojure.string/split version #"\.")]
                            (if (>= (java.lang.Integer/parseInt major) 9)
                              ["--add-modules" "java.xml.bind"]
                              []))}
-   :dev  {:plugins [[com.cemerick/austin "0.1.6"]]
-          :dependencies [[criterium "0.4.2"]]
+   :dev  {:dependencies [[clj-async-test "0.0.5"]
+                         [criterium "0.4.2"]
+                         [eftest "0.5.3"]]
           :repl-options {:init (require '[reagi.core :as r]
                                         '[clojure.core.async
-                                          :as a :refer [go go-loop <! >! <!! >!!]])}}
-   :test {:plugins [[lein-doo "0.1.10"]]
-          :dependencies [[clj-async-test "0.0.5"]]
+                                          :as a :refer [go go-loop <! >! <!! >!!]])}
           :doo     {:build "test-build"}
           :cljsbuild
           {:builds ^:replace {:test-build {:source-paths ["src/cljc" "test/cljs"]
                                            :compiler     {:output-to     "target/test.js"
                                                           :main          reagi.runner
-                                                          :optimizations :simple}}}}}}
+                                                          :optimizations :simple}}}}}
+   :default [:base :system :user :provided :dev :clj-1.8]}
   :aliases
-  {"test-cljs" ["with-profile" "+test" "doo" "phantom" "once"]
-   "test-all"  ["do" "test," "test-cljs"]})
+  {"test-cljs" ["with-profile" "+cljs" "doo" "phantom" "once"]
+   "test-all"  ["with-profile" "+cljs-1.8" "do" "test," "test-cljs"]})
